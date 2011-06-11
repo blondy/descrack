37729a8417495ea0706064f3dbfba2f1709d7db4
Piotr Piechal <piechalp@gmail.com>
2011-06-11 17:50:35 +0200

    Made some variables global

--- descrack-table/main.cpp
+++ descrack-table/main.cpp
@@ -1,24 +1,30 @@
 #include <rainbowtable.h>
 #include <chaingenerator.h>
 
-char** readChainFromFile(FILE* file){
-    char** chain = new char[2];
-    chain[0] = malloc(10);
-    chain[1] = malloc(10);
-    fread(chain[0], 1, 10, file);
-    fread(chain[1], 1, 10, file);
-    return chain;
+FILE * sourceFile;
+char** chain = new char[2];
+int* params = new int[5];
+char* alphabet;
+
+void readChainFromFile(){
+    fread(chain[0], 1, 10, sourceFile);
+    fread(chain[1], 1, 10, sourceFile);
 }
 
-int* readChainCountFromFile(FILE* file){
-    int* count = malloc(sizeof(int));
-    fread(count, 1, sizeof(int), file);
-    return count;
+void readParamstFromFile(){
+    fread(params[0], sizeof(int), 1, sourceFile); //chain_count
+    fread(params[1], sizeof(int), 1, sourceFile); //alphabet_length
+    fread(alphabet, sizeof(char), params[1], sourceFile); //alphabet
+    fread(params[2], sizeof(int), 1, sourceFile); //min_len
+    fread(params[3], sizeof(int), 1, sourceFile); //max_len
+    fread(params[4], sizeof(int), 1, sourceFile); //chain_length
 }
 
 int main(int argc, char *argv[])
 {
-    FILE * source;
+    chain[0] = malloc(10);
+    chain[1] = malloc(10);
+
     if(argc < 2){
         println("No source file specified!");
         return -1;
