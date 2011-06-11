59ceccd8380598c565d21cf1ac69c1ef7c44a5e2
Piotr Piechal <piechalp@gmail.com>
2011-06-11 22:35:38 +0200

    Added some comments and error handling

--- descrack-table/main.cpp
+++ descrack-table/main.cpp
@@ -1,6 +1,7 @@
 #include <rainbowtable.h>
 #include <chaingenerator.h>
 #include <stdlib.h>
+#include <string.h>
 
 FILE * sourceFile;
 char** chain = new char*[2];
@@ -23,32 +24,42 @@ void readParamsFromFile(){
 
 int main(int argc, char *argv[])
 {
-    chain[0] = (char*)malloc(10); //plain1
-    chain[1] = (char*)malloc(10); //plain2
-    char* h_res_salt = new char[3];
-    char* h_res_text = new char[10];
-    char* p_res_salt = new char[3];
-    char* p_res_text = new char[10];
+    if(argc < 3){
+        printf("Invalid parameters!\n");
+        return -1;
+    }
 
     sourceFile = fopen(argv[2], "rb");
+    if(sourceFile == null){
+        printf("An error during opening file has occured!\n");
+        return -1;
+    }
+
+    chain[0] = (char*)malloc(10); //plain1
+    chain[1] = (char*)malloc(10); //plain2
+    char* h_res_salt = new char[3]; //hash auxiliary char arrays
+    char* h_res_text = new char[10];
+    char* p_res_salt = new char[3]; //plain1 auxiliary char arrays
+    char* p_res_text = new char[10];
+
     readParamsFromFile();
 
     ChainGenerator* chainGen = new ChainGenerator(alphabet, params[2], params[3], params[4]);
     char* transformedHash = argv[1];
     int hashReductionCounter = 0;
     int checkedChainsCounter = 0;
-    while(hashReductionCounter < params[4]){
+    while(hashReductionCounter < params[4]){ //while hash reduction chain is shorter that chain_len
         chainGen->reduce(transformedHash, hashReductionCounter++, h_res_salt, h_res_text, true);
-        while(checkedChainsCounter++ < params[0]){
+        while(checkedChainsCounter++ < params[0]){ //while not every chain in file is checked
             readChainFromFile();
             if(strcmp(strcat(h_res_salt, h_res_text), chain[1]) == 0){ //match found
                 int plainReductionCounter = 0;
                 char* transformedPlain1 = chain[0];
-                while(plainReductionCounter < params[4]){
+                while(plainReductionCounter < params[4]){ //while plain1 reduction chain is shorter than chain_len
                     if(strcmp(transformedPlain1, argv[1]) == 0){ //right match found
-                        printf(chain[0]);
+                        printf("%s\n", chain[0]);
                         return 0;
-                    } else {
+                    } else { //reduce and crypt plain1
                         chainGen->reduce(transformedPlain1,plainReductionCounter++, p_res_salt, p_res_text, true);
                         transformedPlain1 = crypt(p_res_salt, p_res_text);
                     }
